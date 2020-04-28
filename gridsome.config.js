f5ba563994dc0793d1fe478d3c6c2c7d40244b57
anekenonso <okenwanonsokenneth@gmail.com>
2020-04-28 23:25:26 +0100

    initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,30 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Anekenonso.codes',
+  siteDescription: "Okenwa kenneth is a software developer, based in Enugu, Nigeria",
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+        route: '/blog/:slug'
+      }
+    }
+  ],
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  },
+}
---
+++ src/layouts/Default.vue
@@ -0,0 +1,45 @@
+<template>
+  <main class="layout" role="main">
+    <slot/>
+    <div class="footer">
+      <p>
+        Built with
+        <a class="link" href="//gridsome.org">Gridsome</a>
+        & Made with ❤️ by
+        <a class="link" href="//twitter.com/anekenonso1_">okenwa kenneth</a>
+      </p>
+    </div>
+  </main>
+</template>
+
+<style>
+body {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+  border: 15px solid rgb(9, 67, 100);
+  min-height: 100vh;
+}
+
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
+.footer {
+  padding-top: calc(30em / 4);
+  text-align: center;
+  font-size: 1em;
+}
+.link {
+  border: 2px dashed rgb(9, 67, 100);
+  padding: 7px;
+  color: #333;
+  text-decoration: none;
+}
+</style>
+S
